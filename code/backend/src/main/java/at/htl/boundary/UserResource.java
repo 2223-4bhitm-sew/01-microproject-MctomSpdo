e7963acb094d83a342023507bab90c33e5210b55
MctomSpdo <mctom.spdo@gmail.com>
2022-11-12 22:22:47 +0100

    implement db

--- code/backend/src/main/java/at/htl/boundary/UserResource.java
+++ code/backend/src/main/java/at/htl/boundary/UserResource.java
@@ -5,9 +5,13 @@ import at.htl.entity.User;
 import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -20,8 +24,6 @@ public class UserResource {
     @Inject
     UserRepository userRepository;
 
-    private List<User> users = new LinkedList<>();
-
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<User> findAll() {
@@ -29,7 +31,7 @@ public class UserResource {
     }
 
     @GET
-    @Path("id")
+    @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public User findById(
             @PathParam("id") long id
@@ -47,18 +49,23 @@ public class UserResource {
     }
 
     @POST
+    @Transactional
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response create(User user) {
-        users.add(user);
-        return Response.created(null).build();
+    public Response create(User user, @Context UriInfo uriInfo) {
+        User saved = userRepository.save(user);
+        logger.info(user.getUsername() + " was created");
+        URI location = uriInfo
+                .getAbsolutePathBuilder()
+                .path("" + saved.getId())
+                .build();
+        return Response.created(location).build();
     }
 
     @DELETE
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response delete(User user) {
-        if(users.size() > 0) {
-            users.remove(user);
-        }
+    @Transactional
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response delete(long id) {
+        userRepository.deleteUserById(id);
         return Response.noContent().build();
     }
 }
--- code/backend/src/main/java/at/htl/control/UserRepository.java
+++ code/backend/src/main/java/at/htl/control/UserRepository.java
@@ -2,6 +2,7 @@ package at.htl.control;
 
 import java.util.List;
 import at.htl.entity.User;
+import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -12,6 +13,9 @@ import javax.persistence.TypedQuery;
 public class UserRepository {
     @Inject
     EntityManager em;
+
+    @Inject
+    Logger logger;
 
     public User save(at.htl.entity.User user) {
         return em.merge(user);
@@ -31,5 +35,27 @@ public class UserRepository {
                 .createNamedQuery("User.findByUserName", User.class)
                 .setParameter("NAME", userName);
         return query.getResultList();
+    }
+
+    /**
+     * deletes a given User from the db
+     * @param user User to delete
+     * @return returns true if deleted, false otherwise
+     */
+    public boolean deleteUser(User user) {
+        if(user == null) return false;
+        em.remove(user);
+        return true;
+    }
+
+    /**
+     * deletes a User that has the given ID
+     * @param id id for the user
+     * @return returns true if deleted, false otherwise
+     */
+    public boolean deleteUserById(long id) {
+        var user = em.find(User.class, id);
+        logger.info(user);
+        return deleteUser(user);
     }
 }
